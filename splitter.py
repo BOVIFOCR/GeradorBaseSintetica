63715bf4669f814191ba58dff43fa9d742651cc7
lmlw19 <lasse1999st@gmail.com>
2022-06-12 18:39:00 -0300

    [chg] adding the tag parsing to splitter

--- splitter.py
+++ splitter.py
@@ -3,7 +3,7 @@ import sys
 import json
 import ast
 
-label_path = './labels/'
+label_path = './label_aux/'
 
 with open(sys.argv[1]) as fd:
     csr = csv.reader(fd)
@@ -13,6 +13,22 @@ headers = ls[0]
 ls = ls[1:]
 fs = {}
 
+def parse_tag(tag):
+    if tag == 'nome' or tag.startswith('ass') or tag == 'filiacao':
+        return 'name'
+    elif tag == 'data-nascimento' or tag == 'data-expedicao':
+        return 'date'
+    elif tag == 'naturalidade':
+        return 'city'
+    elif tag == 'obs' or tag == 'org' or tag == 'cpf' or tag == 'rg' or tag == '5-code' or tag == 'comarca':
+        return tag
+    elif tag == 'doc-origem':
+        return 'doc'
+    elif tag == 'fator-rh':
+        return 'rh'
+    elif tag == 'protocol' or tag == 'unknown':
+        return 'unknown'
+
 for line in ls:
     if line[0] not in fs:
         fs[line[0]] = []
@@ -20,6 +36,13 @@ for line in ls:
     data[headers[4]] = int(line[4])
     data[headers[5]] = ast.literal_eval(line[5])
     data[headers[6]] = ast.literal_eval(line[6])
+    tag = data[headers[6]]['tag']
+    if tag.startswith('info') or tag == 'meta':
+        data[headers[6]]['text_type'] = 'd'
+        data[headers[6]]['info_type'] = 'd'
+    else:
+        data[headers[6]]['text_type'] = 'p'
+        data[headers[6]]['info_type'] = parse_tag(tag)
 
     fs[line[0]].append(data)
 
